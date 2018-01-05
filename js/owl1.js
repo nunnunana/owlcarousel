d55609fb620b0c84ef55bdd4aa21f3800bc735b8
seromkim1005 <seromkim1005@gmail.com>
2018-01-05 21:33:27 +0900

    css 적용

---
+++ js/owl1.js
@@ -0,0 +1,53 @@
+$(document)
+  .ready(function () {
+  $('.owl-carousel.service').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: true,
+        loop: false
+      }
+    }
+  })
+  // responsive nav
+  var responsiveNav = $('#toggle-nav');
+  var navBar = $('.nav-bar');
+
+  responsiveNav.on('click', function (e) {
+    e.preventDefault();
+    console.log(navBar);
+    navBar.toggleClass('active')
+  });
+
+  // pseudo active
+  if ($('#docs').length) {
+    var sidenav = $('ul.side-nav').find('a');
+    var url = window
+      .location
+      .pathname
+      .split('/');
+    var url = url[url.length - 1];
+
+    sidenav.each(function (i, e) {
+      var active = $(e).attr('href');
+
+      if (active === url) {
+        $(e)
+          .parent('li')
+          .addClass('active');
+        return false;
+      }
+    });
+  }
+  });
--- js/owl2.js
+++ js/owl2.js
@@ -1,29 +1,25 @@
 $(document)
   .ready(function () {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 3,
-        nav: false
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: false
+    $('.owl-carousel.introduce').owlCarousel({
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 2,
+          nav: false
+        },
+        1000: {
+          items: 4,
+          nav: true,
+          loop: false
+        }
       }
-    }
-  })
-  });
-
-$(document)
-  .ready(function () {
+    })
     // responsive nav
     var responsiveNav = $('#toggle-nav');
     var navBar = $('.nav-bar');
@@ -54,8 +50,4 @@ $(document)
         }
       });
     }
-
   });
-
-hljs.configure({tabReplace: '  '});
-hljs.initHighlightingOnLoad();
